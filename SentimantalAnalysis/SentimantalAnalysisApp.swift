ec685df37a66ecac9cc75298256b2624ec7212c2
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-25 09:23:38 +0530

    Initial Commit

---
+++ SentimantalAnalysis/SentimantalAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimantalAnalysisApp.swift
+//  SentimantalAnalysis
+//
+//  Created by shehan karunarathna on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct SentimantalAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
